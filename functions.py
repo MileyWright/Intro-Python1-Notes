d53e5629f22477a93fb9e0fbf65143b4b210a093
Miley Wright <dishaliekwright@yahoo.com>
2020-06-29 18:07:24 -0400

    modulo

---
+++ functions.py
@@ -0,0 +1,7 @@
+# define a doubling function
+# takes an integer and returns a value doubled
+def double(x):
+    return x * 2
+
+# define a list of numbers
+lis = []
---
+++ modulo.py
@@ -0,0 +1,20 @@
+# The '%' sign in Python (and most languages)
+# represents the modulo operator
+# You might also hear it called the 'remainder' operator
+
+# What is the remainder that we get when we divide 4 by 27
+print(4 % 2) #0
+
+print(3 % 2) #1
+
+# What's the remainder when we divide 5 by 11?
+print(5 % 11) #5
+
+print(2 % 10) #2
+
+# In Python, if we have the left argument of the modulo 
+# operator be smaller than the right argument, then
+# we just get the left argument back
+
+# One use-case: figuring out if a number is odd or even
+print(num % 2)
